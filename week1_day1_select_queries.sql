f2a0a1825521d9e3fd02c240ac6e71c5d1459632
navyanmekala <navyanmekala@gmail.com>
2025-04-09 14:20:31 +0530

    Create week1_day1_select_queries.sql
    Added SELECT queries for Day 1

---
+++ week1_day1_select_queries.sql
@@ -0,0 +1,10 @@
+-- Day 1: SELECT Queries Practice
+
+-- 1. Select all data
+SELECT * FROM Students;
+
+-- 2. Select name and grade
+SELECT Name, Grade FROM Students;
+
+-- 3. Students older than 20
+SELECT * FROM Students WHERE Age > 20;
